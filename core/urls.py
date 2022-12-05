1292092648b5fbb460e09b324571fd723d490368
YJGS-dev <yordi-futbol@hotmail.com>
2022-12-05 08:57:45 -0600

    Apis

--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from apps.articulo.views import articulo_screen_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', articulo_screen_view, name = 'inicio')
+    path('', articulo_screen_view, name = 'inicio'),
+
+    #rest_framework
+    path('api/articulo/', include('apps.articulo.api.urls','articulo_api')),
 ]
